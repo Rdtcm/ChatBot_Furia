23e2328ccc48ff3281b2a02faa6c0d1f60619ae6
Ryan <ledoryan42@gmail.com>
2025-04-27 01:15:15 -0300

    Troquei o tema do chat para dark e comecei a resolver o bug do chat que nao envia a mensagem pro backend

--- chat/urls.py
+++ chat/urls.py
@@ -6,7 +6,7 @@ app_name = 'Chat'
 
 urlpatterns = [
     path('', views.chat_view, name='chat'),
-    # path('api/messages/<int:chat_id>/', views.get_messages, name='get_messages'),
-    # path('api/send/', views.send_message, name='send_message'),
-    # path('api/chats/', views.get_previous_chat, name='get_previous_chats'),
+    path('api/messages/<int:chat_id>/', views.get_messages, name='get_messages'),
+    path('api/send/', views.send_message, name='send_message'),
+    path('api/chats/', views.get_previous_chat, name='get_previous_chats'),
 ]
